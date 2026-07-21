3066d029de8a524c9c98a1d2d820090d8a1feb93
Case Walker <25988138+casewalker@users.noreply.github.com>
2026-07-21 16:52:42 -0400

    Update KitchenSink rag-api jwt-secret location to avoid colliding with Dev (#585)
    As it stands, KitchenSink and Dev are pulling the same rag-api JWT_SECRET value, but the environments configure different values in LibreChat. This PR should update the KitchenSink stack to pull a different Secrets Manager secret and thus allow the environments to have different values.

--- nj/infra/bin/cdk.ts
+++ nj/infra/bin/cdk.ts
@@ -163,7 +163,7 @@ if (process.env.DEPLOY_KITCHENSINK === 'true') {
     listenerArn: ecsStack.listener.listenerArn,
     certificateArn: `arn:aws:acm:${env.region}:${env.account}:certificate/${process.env.LIBRECHAT_ACM_CERTIFICATE_ID}`,
     mongoSecretArn: `arn:aws:secretsmanager:${env.region}:${env.account}:secret:ai-assistant/kitchensink/mongodb-B2athN`,
-    ragApiJwtSecretArn: `arn:aws:secretsmanager:${env.region}:${env.account}:secret:ai-assistant/rag-api/jwt-secret-${ragApiJwtSecretSuffix[AWS_ENV]}`,
+    ragApiJwtSecretArn: `arn:aws:secretsmanager:${env.region}:${env.account}:secret:ai-assistant/kitchensink/rag-api-jwt-secret`,
   });
 
   applyTags(kitchenSinkStack);
